d3035552f26e00dc4d555fd5abf401e8465a3fae
Eric Ackerson <eric.ackerson@getupside.com>
2023-02-01 13:10:44 -0500

    oops

--- source/alias.sh
+++ source/alias.sh
@@ -4,8 +4,8 @@ alias conformity="source $(pwd)/conformity.sh"
 #k8s
 #install kube-ps1 via https://github.com/jonmosco/kube-ps1
 source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
-alias k8s_context='PS1="$(kube_ps1)"$PS1'
+alias k8s-context='PS1="$(kube_ps1)"$PS1'
 
 #git
-alias oops='git commit -am "oops" && git rebase -i HEAD~2 && git push --force origin $(git rev-parse --abbrev-ref HEAD)'
-alias git-master='git checkout master && git rebase origin master'
+alias oops='git commit -am "oops" && git rebase HEAD~2 && git push --force origin $(git rev-parse --abbrev-ref HEAD)'
+alias git-master='git checkout master && git pull --rebase origin master'
