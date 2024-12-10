7798c07021cfc42d0f3bed4a89a4e945f9e0e81a
Ackertron <eric.ackerson@upside.com>
2024-12-10 15:30:05 -0500

    updates

--- source/alias.sh
+++ source/alias.sh
@@ -7,5 +7,7 @@ source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
 alias k8s-context='PS1="$(kube_ps1)"$PS1'
 
 #git
-alias oops='git commit -am "oops" && git rebase HEAD~2 && git push --force origin $(git rev-parse --abbrev-ref HEAD)'
+alias oops='git commit -am "oops" && git rebase -i HEAD~2 && git push --force origin $(git rev-parse --abbrev-ref HEAD)'
 alias git-master='git checkout master && git pull --rebase origin master'
+alias git-main='git checkout main && git pull --rebase origin main'
+alias git-clean="git branch | grep -v 'master' | grep -v 'main' | xargs git branch -D"
